8f0d9ee26486af3557e90a7660eb5d14eb014622
Vladimir Severov <sevvova@inbox.ru>
2019-02-09 00:48:33 +0300

    Function 'zip' was optimized. JSDoc added.

--- source/zip.js
+++ source/zip.js
@@ -1,3 +1,11 @@
 'use strict';
 
-const zip = (...objects) => Object.assign.apply(null, [].reverse.call(objects));
+/**
+ * Concatenates objects into one. When repeating properties, the first mentioned value is used.
+ * @param {Object[]} objects - The objects that will concatenate.
+ * @return {Object} Result of concatenation.
+ */
+const zip = (...objects) => objects.reduceRight((res, obj) => Object.assign(res, obj), {});
+
+// Немного исправленный старый вариант:
+// const zip = (...objects) => Object.assign(...[].reverse.call(objects));
